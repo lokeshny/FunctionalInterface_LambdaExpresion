43a486676a9ed111a5526cb7a0e07e7b892d8db2
lokeshny <lokeshny97@gmail.com>
2022-05-13 19:19:25 +0530

    Added:email validator

--- Main.java
+++ Main.java
@@ -10,34 +10,48 @@ class UserDetails {
     Validator validate =  new Validator();
 
     /* Creating user details variables and making them private*/
-    private String fName;
-    private String lName;
+    private String F_Name;
+    private String L_Name;
+    private String emailId;
 
     /* Creating getters and setters for user details variables*/
 
     public String getfName() {
-        return fName;
+        return F_Name;
     }
 
     public void setfName(String fName) {
         // Validating fName
         if(validate.validateFirstName(fName)) {
-            this.fName = fName;
+            this.F_Name = fName;
         } else {
             System.out.println("Invalid fName");
         }
     }
 
     public String getlName() {
-        return lName;
+        return L_Name;
     }
 
     public void setlName(String lName) {
         // Checking lName
         if(validate.validateLastName(lName)) {
-            this.lName = lName;
+            this.L_Name = lName;
         } else {
             System.out.println("Invalid lName");
+        }
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        // Checking emailId
+        if(validate.validateEmailId(emailId)) {
+            this.emailId = emailId;
+        } else {
+            System.out.println("Invalid emailId");
         }
     }
 }
@@ -46,10 +60,12 @@ public class Main {
         // Creating object of user details class
         UserDetails user = new UserDetails();
         // Calling methods
-        user.setfName("Sarvesh");
-        user.setlName("Pednekar");
+        user.setfName("Lokesh");
+        user.setlName("NY");
+        user.setEmailId("lokeshny97@gamil.com");
 
         System.out.println(user.getfName());
         System.out.println(user.getlName());
+        System.out.println(user.getEmailId());
     }
 }
