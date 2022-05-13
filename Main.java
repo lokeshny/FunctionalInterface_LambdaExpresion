e03fd2b4e7aa35637b4a041bce3e86d44ab116c5
lokeshny <lokeshny97@gmail.com>
2022-05-13 19:08:51 +0530

    Added: User Details

---
+++ Main.java
@@ -0,0 +1,37 @@
+/* Assignment on user registration using functional interface and lambada expression
+ * RFP assignment
+ * @Author: Lokesh n y
+ * @date:12.05.2022
+ * */
+package com.Bridgelab.FunctionalInterface;
+
+class UserDetails {
+    /* Creating object of validator class*/
+    Validator validate =  new Validator();
+
+    /* Creating user details variables and making them private*/
+    private String fName;
+
+    /* Creating getters and setters for user details variables*/
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        // Validating fName
+        if(validate.validateFirstName(fName)) {
+            this.fName = fName;
+        } else {
+            System.out.println("Invalid fName");
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        // Creating object of user details class
+        UserDetails user = new UserDetails();
+        // Calling method
+        user.setfName("Sarvesh");
+        System.out.println(user.getfName());
+    }
+}
---
+++ Validator.java
@@ -0,0 +1,14 @@
+package com.Bridgelab.FunctionalInterface;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    private final String FIRST_NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+
+        public boolean validateFirstName(String fName) {
+            boolean result = Pattern.matches(FIRST_NAME_PATTERN, fName);
+            return result;
+        }
+    }
+
+
